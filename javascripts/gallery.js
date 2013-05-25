b096068946bd75f574e240af3e90d7fe4abb5651
Jasper den Ouden <o.jasper@gmail.com>
2013-05-25 03:27:22 +0200

    Separated out some of the gallery .js

---
+++ javascripts/gallery.js
@@ -0,0 +1,27 @@
+// Copyright (C) 22-05-2013 Jasper den Ouden. GPLv3
+
+var cur_gal_index = 1;
+function to_img_re_url(index)
+{   var to_ind = location.href.indexOf("#");
+    if( to_ind != -1 )
+    {   location.href = location.href.substr(0,to_ind) + "#img_" + index; }
+    else
+    {   location.href = location.href + "#img_" + index; }
+    to_img(index)
+}
+
+var designs =[]; //Dictionary of designs.
+
+function to_img(index)
+{   cur_gal_index = index;
+    var file = imgs[index];
+    var ext  = file.substr(file.length-4);
+    if( ext==".jpg" || ext==".png" || ext==".gif" ) //Put an image there.
+    {   document.getElementById("viewer").src = file;
+    }
+    else if( ext==".stl" || ext==".obj" ) //Disable image, put design.
+    {    js_import(file + ".js");
+         thingiview.loadArray(designs[file]);
+         document.getElementById("viewer").src = null;
+    }
+}
