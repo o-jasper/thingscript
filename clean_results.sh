a56804fb7ff6b2a014bdbe556e4a7ad48312ba71
Jasper den Ouden <o.jasper@gmail.com>
2013-05-29 21:46:25 +0200

    Added a if_newer and got stuff to fit either if_newer or memoize. Costs is that stuff needs tiny scripts

--- clean_results.sh
+++ clean_results.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #Delete stuff so the check if it is actually newer doesnt hide bugs.
 
-rm testpage/page.html
-cd testpage/output/
-rm *.js *.png *.pov
+rm testpage/index.html
+rm -r testpage/output/tso
